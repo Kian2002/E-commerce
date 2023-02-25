78dfbce69c196a0963824b8583275be836f68f20
Kian2002 <canadakurdistan@gmail.com>
2023-02-25 00:18:48 -0800

    create hero and add background to home page

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={styles["hero"]}>
+      <h1 className={styles["hero__heading"]}>Welcome!</h1>
+      <p className={styles["hero__p"]}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eos
+        fuga esse reprehenderit aliquam velit quis adipisci aliquid quaerat.
+        Enim eligendi corporis aperiam nesciunt recusandae veritatis odio
+        reprehenderit animi nam?
+      </p>
+    </div>
+  );
 };
 
 export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
+import styles from "./styles/Home.module.css";
 
 const Home = () => {
   return (
     <>
+      <div className={styles["bg-img"]} />
       <Navbar />
       <Hero />
     </>
