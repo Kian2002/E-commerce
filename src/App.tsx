11112a8f1d70ff82e672476b11b541bc0f338ee3
Kian2002 <canadakurdistan@gmail.com>
2023-02-24 21:02:35 -0800

    set up routing and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Store from "./pages/Store";
+import Item from "./pages/Item";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <div>
-      <h1>TEST</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="store" element={<Store />} />
+        <Route path="store/item/:itemId" element={<Item />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
