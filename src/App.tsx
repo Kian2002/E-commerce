110fd3d29bdbbd5e942e03f6d6c142b93c01ed78
Kian2002 <canadakurdistan@gmail.com>
2023-02-25 21:58:43 -0800

    setup framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Store from "./pages/Store";
 import Item from "./pages/Item";
 import ErrorPage from "./pages/ErrorPage";
 import Navbar from "./components/navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="store" element={<Store />} />
-        <Route path="store/item/:itemId" element={<Item />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="store" element={<Store />} />
+          <Route path="store/item/:itemId" element={<Item />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
