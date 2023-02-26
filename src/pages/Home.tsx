9aa309c426859359cf74708ee66a0dce52b3e29a
Kian2002 <canadakurdistan@gmail.com>
2023-02-25 22:54:34 -0800

    home and store animations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
 import styles from "./styles/Home.module.css";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <>
-      <div className={styles["bg-img"]} />
+    <motion.div
+      className={styles["bg-img"]}
+      initial={{ opacity: 0, y: -175 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { y: { type: "spring", duration: 0.9, bounce: 0.5 } },
+      }}
+      exit={{
+        opacity: 0,
+        transition: { y: { type: "tween", duration: 0.6, bounce: 0.3 } },
+      }}
+    >
       <Hero />
-    </>
+    </motion.div>
   );
 };
 
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,13 +3,27 @@ import Navbar from "../components/navbar/Navbar";
 import Sidebar from "../components/sidebar/Sidebar";
 import StoreMain from "../components/main/StoreMain";
 import styles from "./styles/Store.module.css";
+import { motion } from "framer-motion";
 
 const Store = () => {
   return (
-    <div className={styles["store"]}>
+    <motion.div
+      className={styles["store"]}
+      initial={{ opacity: 1, y: 150 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: { y: { type: "spring", duration: 0.9, bounce: 0.4 } },
+      }}
+      exit={{
+        opacity: 0,
+        y: 150,
+        transition: { y: { type: "tween", duration: 0.4, bounce: 0.3 } },
+      }}
+    >
       <Sidebar />
       <StoreMain />
-    </div>
+    </motion.div>
   );
 };
 
