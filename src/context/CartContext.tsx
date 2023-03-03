75082817823bf7b5df8648c011ddf6f59792fe0e
Kian2002 <canadakurdistan@gmail.com>
2023-03-03 12:19:22 -0800

    create cart context

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+import { Games } from "../data";
+
+type CartContextType = {
+  cart: Games[];
+  addToCart: (game: Games) => void;
+  removeFromCart: (game: Games) => void;
+};
+
+const CartContext = createContext<CartContextType>({
+  cart: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const CartProvider: React.FC<Props> = ({ children }) => {
+  const [cart, setCart] = useState<Games[]>([]);
+
+  const addToCart = (game: Games) => {
+    setCart([...cart, game]);
+  };
+
+  const removeFromCart = (game: Games) => {
+    setCart(cart.filter((item) => item.id !== game.id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartProvider, CartContext };
