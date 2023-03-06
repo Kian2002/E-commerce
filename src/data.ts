39fad016729cf281c91a28c5cae6b8832399a04e
Kian2002 <canadakurdistan@gmail.com>
2023-03-05 21:42:02 -0800

    fix types & add more data

--- src/data.ts
+++ src/data.ts
@@ -1,15 +1,55 @@
+type Developers = {
+  games_count: number
+  id: number
+  image_background: string
+  name: string
+  slug: string 
+}
+
+type Publishers = {
+  games_count: number
+  id: number
+  image_background: string
+  name: string
+  slug: string
+}
+
+type Platforms = {
+  platform: {
+    games_count: number
+    id: number
+    image_background: string
+    name: string
+    slug: string
+    year_end: number
+    year_start: number
+  }
+  released_at: string
+  requirements_en: null
+  requirements_ru: null
+}
+
+type Genres = {
+  games_count: number
+  id: number
+  image_background: string
+  name: string
+  slug: string
+}
+
 export type Games = {
   id: number;
   name: string;
   price: number;
   link: string;
   released: string;
-  platforms: any[];
-  genres: any[];
-  developers: string[];
+  platforms: Platforms[];
+  genres: Genres[];
+  developers: Developers[];
   publishers: string[];
   background_image: string;
   short_screenshots: string[];
+  description: string;
 };
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -27,6 +67,8 @@ const getData = async () => {
       platforms: game.platforms,
       genres: game.genres,
       background_image: game.background_image,
+      short_screenshots: game.short_screenshots,
+      released: game.released
     };
   });
   return storeFilter;
