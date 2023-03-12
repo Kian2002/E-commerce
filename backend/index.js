0e2c0e29afc09becaf4aeef5112f09046d675a2d
Kian2002 <canadakurdistan@gmail.com>
2023-03-12 01:35:17 -0800

    losing mind p5

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// const PORT = process.env.PORT ? process.env.PORT : "3000";
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => console.log("Server started"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
