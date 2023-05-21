450077a7eb5bf63f32897438a672c97d43443ab4
Kian2002 <canadakurdistan@gmail.com>
2023-05-21 12:40:32 -0700

    UI fixes

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,10 +3,10 @@ import styles from "./Hero.module.css";
 const Hero = () => {
   return (
     <div className={styles["hero"]}>
-      <h1 className={styles["hero__heading"]}>Welcome!</h1>
+      <h1 className={styles["hero__heading"]}>Welcome to the Game Store!</h1>
       <p className={styles["hero__p"]}>
-        Welcome to the Game Store! Here you can find all the latest games and
-        their information. Click on the store button to get started!
+        Here you can find all the latest games and their information. Click on
+        the store button to get started!
       </p>
     </div>
   );
