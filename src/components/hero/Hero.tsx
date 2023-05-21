35b519ee29ce92e951f402f0158fb565d7fc9f03
Kian2002 <canadakurdistan@gmail.com>
2023-05-20 22:55:10 -0700

    update hero text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,10 +5,8 @@ const Hero = () => {
     <div className={styles["hero"]}>
       <h1 className={styles["hero__heading"]}>Welcome!</h1>
       <p className={styles["hero__p"]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eos
-        fuga esse reprehenderit aliquam velit quis adipisci aliquid quaerat.
-        Enim eligendi corporis aperiam nesciunt recusandae veritatis odio
-        reprehenderit animi nam?
+        Welcome to the Game Store! Here you can find all the latest games and
+        their information. Click on the store button to get started!
       </p>
     </div>
   );
